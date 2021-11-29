b1f5011c581029f45c973aa8a5fcd89417e19762
hjh <38830650+hjhroyal@users.noreply.github.com>
2021-11-29 20:53:50 +0800

    Add files via upload

---
+++ server/Sql/sql_initial.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+
+class MyDatabase:
+    """
+    这个类主要用于数据库的初始化
+    """
+    db = pymysql.Connect
+
+    def __init__(self, host, user, password, database):
+        self.db = pymysql.connect(host=host,
+                                  user=user,
+                                  password=password,
+                                  database=database)
