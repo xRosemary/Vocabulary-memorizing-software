c13bed6b0a4d706030bfe6c81e0903478d036522
hjh <38830650+hjhroyal@users.noreply.github.com>
2021-12-28 19:53:44 +0800

    Add files via upload

--- static/js/newindex.js
+++ static/js/newindex.js
@@ -2,7 +2,9 @@ let word_list = ["aaa"]
 
 
 $("#start").click(function (e) {
-    word_list = $("#input_area").val();
-
     location.href = "learn"
+})
+
+$("#getBoxPic").click(function (e) {
+    location.href = "/login/getDataPic"
 })
